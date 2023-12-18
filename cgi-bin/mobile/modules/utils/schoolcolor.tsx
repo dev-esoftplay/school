fca064f22b87644d6cd1e7edcf65e3b0504f836e
Yasin <yasinmuhammadyusuf321@gmail.com>
2023-12-18 14:38:32 +0700

    YSN oboard-auth

---
+++ cgi-bin/mobile/modules/utils/schoolcolor.tsx
@@ -0,0 +1,13 @@
+// SchoolColors.ts
+
+class SchoolColors {
+  primary: string;
+  secondary: string;
+
+  constructor() {
+    this.primary = '#146C94';
+    this.secondary = '#19A7CE';
+  }
+}
+
+export default SchoolColors;
