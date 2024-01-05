01345795a06fe8c90115e8d9fbc97310ac79bbfa
agungpradana7 <gofudgang@gmail.com>
2024-01-05 15:29:43 +0700

    Agung : Trying API

--- modules/api/teacher.php
+++ modules/api/teacher.php
@@ -1,74 +1,104 @@
-<?php  if (!defined('_VALID_BBC')) exit('No direct script access allowed');
+<?php if (!defined('_VALID_BBC')) exit('No direct script access allowed');
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 	$teacher = $db->getassoc("SELECT * FROM school_teacher WHERE 1");
 	return api_ok($teacher);
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") // HANDLE INSERT DATA FROM INPUT MANUAL DATA
-{
-  if (isset($_POST['submit']) && $_POST['submit'] == 'submit_form1') {
-    $_POST[$item];
-  } elseif (isset($_POST['submit']) && $_POST['submit'] == 'submit_form2') {
-    unset($_POST[$item]);
-  }
+// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+// 	// $teacher_name = $_POST['nama_guru'];
 
-  if (!empty($_POST['nama_guru']) && !empty($_POST['nip']) && !empty($_POST['phone']) && !empty($_POST['position'])) {
-    $guru_user_id = $db->Insert('bbc_user', array(
-      'password'  => $password,
-      'username'  => $data['nip'],
-    ));
+// 	// $db->Insert('school_teacher', array(
+// 	// 	'name' => $teacher_name,
+// 	// ));
 
-    $db->insert('bbc_account', array(
-      'user_id'   => $guru_user_id,
-      'username'  => $data['nip'],
-      'name'      => $data['nama_guru']
-    ));
+// 	$input_data = file_get_contents("php://input");
 
-    $db->insert('school_teacher', array(
-      'user_id'   => $guru_user_id,
-      'name'      => $data['nama_guru'],
-      'nip'       => $data['nip'],
-      'phone'     => $data['phone'],
-      'position'  => $data['position']
-    ));
-  }
 
-  if (!empty($_FILES['file']) && (!empty($_POST) || isset($_POST))) {
-    $output = _lib('excel')->read($_FILES['file']['tmp_name'])->sheet(1)->fetch();
-    unset($output[1]);
-    foreach ($output as $key => $value) {
-      $password = encode(!empty($value[$data['nama_guru']]));
-      $q = $db->getOne("SELECT username FROM bbc_user WHERE username = '" . $value[$data['nip']] . "'");
-      if (!$q) {
-        $db->Insert('bbc_user', array(
-          'username'  => $value[$data['nip']],
-          'password'  => $password,
-        ));
-      }
+// 	$guru_user_id = $db->Insert('bbc_user', array(
+// 		// 'password'  => $password,
+// 		'username'  => $_POST['nip'],
+// 	));
 
-      $r  = $db->getOne("SELECT username FROM bbc_account WHERE username = '" . $value[$data['nip']] . "'");
-      $y  = $db->getOne("SELECT `id` FROM `bbc_user` WHERE username = '" . $value[$data['nip']] . "'");
+// 	$db->insert('bbc_account', array(
+// 		'user_id'   => $guru_user_id,
+// 		'username'  => $_POST['nip'],
+// 		'name'      => $_POST['nama_guru']
+// 	));
 
-      if (!$r) {
-        $db->Insert('bbc_account', array(
-          'user_id'  => $y,
-          'username' => $value[$data['nip']],
-          'name'     => $value[$data['nama_guru']],
-        ));
-      }
+// 	$db->insert('school_teacher', array(
+// 		'user_id'   => $guru_user_id,
+// 		'name'      => $_POST['nama_guru'],
+// 		'nip'       => $_POST['nip'],
+// 		'phone'     => $_POST['phone'],
+// 		'position'  => $_POST['position']
+// 	));
 
-      $s  = $db->getOne("SELECT nip FROM school_teacher WHERE nip = '" . $value[$data['nip']] . "'");
-      if (!$s) {
-        $db->Insert('school_teacher', array(
-          'user_id'  => $y,
-          'name'     => $value[$data['nama_guru']],
-          'nip'      => $value[$data['nip']],
-          'phone'    => $value[$data['phone']],
-          'position' => $value[$data['position']],
-        ));
-      }
-    }
-  }
-	api_ok($teacher);
-}
+// 	$result = [
+// 		'user_id'  => $guru_user_id,
+// 		'name' 		 => $_POST['nama_guru'],
+// 		'nip' 		 => $_POST['nip'],
+// 		'phone'		 => $_POST['phone'],
+// 		'position' => $_POST['position'],
+// 	];
+
+// 	$api_data = array (
+// 		'user_id'   => $guru_user_id,
+// 		'nama_guru' => $_POST['nama_guru'],
+// 		'nip' 			=> $_POST['nip'],
+// 		'nip' 			=> $_POST['nip'],
+// 		'position' 	=> $_POST['position'],
+// 	);
+
+// 	$api = 'http://api.school.lc/teacher';
+
+// 	$ch  = curl_init($api);
+
+// 	curl_setopt($ch, CURLOPT_POST, 1);
+// 	curl_setopt($ch, CURLOPT_POSTFIELDS, $api_data);
+// 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+// 	$response = curl_exec($ch);
+
+// 	curl_close($ch);
+
+// 	echo $response;
+
+// 	api_ok($result);
+// } else {
+// 	$error = [
+// 		'message' => 'Gagal nambah data'
+// 	];
+// 	api_no($error);
+// }
+
+// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+// 	$data 		= file_get_contents("php://input");	
+// 	$jsonData = json_decode($data, true);
+
+// 	api_ok($jsonData);
+// }
+
+// $url = 'https://api.school.lc/teacher';
+// $data = array(
+// 	'name' => 'Asep',
+// );
+
+// $options = array(
+// 	'http' => array(
+// 		'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+// 		'method'  => 'POST',
+// 		'content' => http_build_query($data),
+// 	),
+// );
+
+// $context  = stream_context_create($options);
+// $result = file_get_contents($url, false, $context);
+
+// if ($result === FALSE) {
+// 	// Handle error
+// 	echo "Gagal melakukan permintaan HTTP POST";
+// } else {
+// 	// Proses hasil dari server
+// 	echo "Data berhasil ditambahkan!";
+// }
