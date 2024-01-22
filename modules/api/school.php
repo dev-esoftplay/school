3d3a76a85134b89c4df99d297bce81fbfe9763ac
agungpradana7 <gofudgang@gmail.com>
2024-01-22 14:39:27 +0700

    Agung : improve api

--- modules/api/school.php
+++
@@ -1,5 +0,0 @@
-<?php  if (!defined('_VALID_BBC')) exit('No direct script access allowed');
-
-	$teacher = $db->cacheGetOne('SELECT name FROM school_teacher WHERE id = '.$teacher);
-	return api_ok($teacher);
-
