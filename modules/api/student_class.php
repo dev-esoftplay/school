03a5c9485682d7be20155a2d6cd0ad7ecc6391e3
IlhamMaulanaaa <xilhammaulanax739@gmail.com>
2024-01-23 14:00:39 +0700

    ilham : fix api student class

--- modules/api/student_class.php
+++ modules/api/student_class.php
@@ -5,7 +5,6 @@ $class_id     = addslashes(intval($_GET['class_id']));
 $schedule_id  = addslashes(intval($_GET['schedule_id'])); 
 
 if (!empty($teacher_id) && !empty($class_id)) {
-	// Ambil seluruh data siswa dari tabel school_student_class berdasarkan class_id
 	$student_data = $db->getall('SELECT ssc.`student_id`, ssc.`number`, ss.`name` 
 																FROM `school_student_class` AS ssc 
 																INNER JOIN `school_student` AS ss ON ssc.`student_id` = ss.`id` 
@@ -13,17 +12,24 @@ if (!empty($teacher_id) && !empty($class_id)) {
 
 	$class_name    = $db->getone('SELECT CONCAT_WS(" ", `grade`, `major`, `label`) FROM `school_class` WHERE `id`=' . $class_id);
 	$schedule_time = $db->getrow('SELECT `clock_start`, `clock_end` FROM `school_schedule` WHERE `id`=' . $schedule_id); 
+	$permission 	 = $db->getRow('SELECT `total_present`, `total_s`, `total_i`, `total_a` FROM `school_attendance_report` WHERE `schedule_id`=' . $schedule_id.' AND `class_id` = '. $class_id.' AND DATE(created) = CURDATE()');
 	foreach ($student_data as &$student) {
 		$student['status'] = 1;
+	}
+	if ($permission)
+	{
+		$permission = array(
+			'total_present' => $permission['total_present'],
+			'total_s'       => $permission['total_s'],
+			'total_i'       => $permission['total_i'],
+			'total_a'       => $permission['total_a'],
+		);
 	}
 
 	$result = array(
 		'class_name'     		=> $class_name,
 		'student_count'	 		=> count($student_data),
-		'student_present'		=> count($student_data),
-		'student_sick'   		=> '0',
-		'student permission'=> '0',
-		'student_absent' 		=> '0',
+		'permission'      	=> $permission,
 		'clock_start'    		=> $schedule_time['clock_start'],
 		'clock_end'      		=> $schedule_time['clock_end'],
 		'student_list'   		=> $student_data,
