73ba5f3f3cbe6d472b5abc5195f338eadca21dbc
IlhamMaulanaaa <xilhammaulanax739@gmail.com>
2024-01-23 13:03:46 +0700

    ilham : fix api student attendance

--- modules/api/student_attendance.php
+++ modules/api/student_attendance.php
@@ -1,33 +1,52 @@
 <?php
 if (!defined('_VALID_BBC')) exit('No direct script access allowed');
 
-$data_post = [ 'student_id','presence', 'notes', 'schedule_id'];
+$data = json_decode($_POST['data'], true);
 
-if ($teacher_id) 
+if ($data === null) 
 {
-  $data = array();
-
-  foreach ($data_post as $value) 
-  {
-    if (isset($_POST[$value])) {
-      $data[$value] = addslashes($_POST[$value]);
-    } else {
-      return api_no(['message' => 'Missing ' . $value . ' in POST data']);
-    }
-  }
-
-  $attendance_data = array(
-    'student_id'  => $data['student_id'],
-    'schedule_id' => $data['schedule_id'],
-    'presence'    => $data['presence'],
-    'notes'       => $data['notes'],
-  );
-
-  // Insert data into the `attendance` table
-  // Make sure you have a valid $db object and a proper connection before this point
-  $db->insert('school_attendance', $attendance_data);
-
-  return api_ok(['message' => 'Data added successfully']);
-} else {
-    return api_no(['message' => 'Unauthorized access. Teacher ID is missing.']);
+    return api_no(['message' => 'Invalid JSON data']);
 }
+
+foreach ($data as $entry) 
+{
+    $required_fields = ['student_id', 'schedule_id', 'presence', 'notes'];
+
+    foreach ($required_fields as $field) 
+    {
+        if (!isset($entry[$field])) 
+        {
+            return api_no(['message' => 'Missing ' . $field . ' in JSON data']);
+        }
+    }
+}
+
+foreach ($data as $entry) 
+{
+  $query          = 'SELECT `id` FROM school_attendance WHERE student_id = \''.$entry['student_id'].'\' AND schedule_id = \''.$entry['schedule_id'].'\' AND DATE(created) = CURDATE()';
+  $result         = $db->execute($query);
+  $existing_data  = $result->fetch_assoc();
+  if ($existing_data) 
+  {
+    $db->update(
+      'school_attendance',
+      [
+        'presence'    => addslashes($entry['presence']),
+        'notes'       => addslashes($entry['notes']),
+      ],
+      $existing_data['id']
+    );
+  }else 
+  {
+    $attendance_data = array(
+      'student_id'  => addslashes($entry['student_id']),
+      'schedule_id' => addslashes($entry['schedule_id']),
+      'presence'    => addslashes($entry['presence']),
+      'notes'       => addslashes($entry['notes']),
+    );
+
+    $db->insert('school_attendance', $attendance_data);
+  }
+}
+return api_ok(['message' => 'Data added or updated successfully']);
+
