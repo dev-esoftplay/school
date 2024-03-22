24e872f547fb04225d97a71091a99df39bc72cfa
IlhamMaulanaaa <xilhammaulanax739@gmail.com>
2024-03-22 14:49:57 +0700

    ilham : add admin parent date

--- modules/school/admin/attendance.php
+++ modules/school/admin/attendance.php
@@ -1,28 +1,33 @@
-<?php  if (!defined('_VALID_BBC')) exit('No direct script access allowed');
-
+<?php if (!defined('_VALID_BBC'))exit ('No direct script access allowed');
 $form = _lib('pea', 'school_attendance_report');
 $form->initSearch();
 
-$form->search->addInput('course_id','selecttable');
+$form->search->addInput('teacher_id', 'selecttable');
+$form->search->input->teacher_id->setTitle('Search by teacher');
+$form->search->input->teacher_id->addOption('Select teacher', '');
+$form->search->input->teacher_id->setReferenceTable('school_teacher');
+$form->search->input->teacher_id->setReferenceField('name', 'id');
+
+$form->search->addInput('course_id', 'selecttable');
 $form->search->input->course_id->setTitle('Search by course');
 $form->search->input->course_id->addOption('Select course', '');
 $form->search->input->course_id->setReferenceTable('school_course');
 $form->search->input->course_id->setReferenceField('name', 'id');
 
-$form->search->addInput('class_id','selecttable');
+$form->search->addInput('class_id', 'selecttable');
 $form->search->input->class_id->setTitle('Search by Class');
 $form->search->input->class_id->addOption('Select Class', '');
 $form->search->input->class_id->setReferenceTable('school_class');
 $form->search->input->class_id->setReferenceField('CONCAT_WS(" ",grade,major,label)', 'id');
 
-$form->search->addInput('keyword','keyword');
+$form->search->addInput('keyword', 'keyword');
 $form->search->input->keyword->addSearchField('total_present,total_s,total_i,total_a,date_day,date_week,date_month,date_year');
 
 $add_sql = $form->search->action();
 $keyword = $form->search->keyword();
 echo $form->search->getForm();
 
-$form->initRoll($add_sql.' ORDER BY id DESC', 'id' );
+$form->initRoll($add_sql . ' ORDER BY id DESC', 'id');
 
 $form->roll->setSaveTool(true);
 
@@ -30,23 +35,32 @@ $form->roll->addInput('id', 'sqlplaintext');
 $form->roll->input->id->setTitle('id');
 $form->roll->input->id->setDisplayColumn(true);
 
+$form->roll->addInput('teacher', 'selecttable');
+$form->roll->input->teacher->setTitle('teacher');
+$form->roll->input->teacher->setFieldName('teacher_id');
+$form->roll->input->teacher->setReferenceTable('school_teacher');
+$form->roll->input->teacher->setReferenceField('name', 'id');
+$form->roll->input->teacher->setPlaintext(true);
+$form->roll->input->teacher->setDisplayColumn(true);
+$form->roll->input->teacher->textTip = '';
+
 $form->roll->addInput('course', 'selecttable');
 $form->roll->input->course->setTitle('course');
-$form->roll->input->course->setFieldName( 'course_id' );
+$form->roll->input->course->setFieldName('course_id');
 $form->roll->input->course->setReferenceTable('school_course');
-$form->roll->input->course->setReferenceField('name','id');
+$form->roll->input->course->setReferenceField('name', 'id');
 $form->roll->input->course->setPlaintext(true);
 $form->roll->input->course->setDisplayColumn(true);
-$form->roll->input->course->textTip='';
+$form->roll->input->course->textTip = '';
 
 $form->roll->addInput('class', 'selecttable');
 $form->roll->input->class->setTitle('class');
-$form->roll->input->class->setFieldName( 'class_id' );
+$form->roll->input->class->setFieldName('class_id');
 $form->roll->input->class->setReferenceTable('school_class');
-$form->roll->input->class->setReferenceField('CONCAT_WS(" ",grade,major,label)','id');
+$form->roll->input->class->setReferenceField('CONCAT_WS(" ",grade,major,label)', 'id');
 $form->roll->input->class->setPlaintext(true);
 $form->roll->input->class->setDisplayColumn(true);
-$form->roll->input->class->textTip='';
+$form->roll->input->class->textTip = '';
 
 $form->roll->addInput('status', 'sqlplaintext');
 $form->roll->input->status->setTitle('Status');
@@ -83,6 +97,14 @@ $form->roll->input->date_month->setDisplayColumn(false);
 $form->roll->addInput('date_year', 'sqlplaintext');
 $form->roll->input->date_year->setTitle('Year');
 $form->roll->input->date_year->setDisplayColumn(false);
+
+$form->roll->addInput('created', 'sqlplaintext');
+$form->roll->input->created->setTitle('Tanggal');
+$form->roll->input->created->setDisplayColumn(true);
+$form->roll->input->created->setDisplayFunction(function ($value) use ($db) {
+  $newFormat = date("d-F-Y", strtotime($value));
+  return $newFormat;
+});
 
 $form->roll->addReport('excel');
 
