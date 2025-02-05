7c66e2a0b42a3b05bff687f7653eb4b6bcd3d2b8
AkbarFelda <akbarfelda65@gmail.com>
2025-02-05 12:05:04 +0700

    fix:[AF] fix deprecated on profile-page

--- modules/teacher/profile/page.html.php
+++ modules/teacher/profile/page.html.php
@@ -2,30 +2,6 @@
 if (!defined('_VALID_BBC')) exit('No direct script access allowed');
 
 $sys->set_layout('teacher.php');
-
-$user_id = intval($user->id); // Pastikan user_id dalam bentuk angka
-$sql = sprintf("SELECT * FROM school_teacher WHERE user_id = %d", $user_id);
-$teacher = $db->getRow($sql); // Ambil data guru berdasarkan user_id
-
-// Pastikan $teacher ada sebelum mengambil gender
-$gender = $teacher['gender'] ?? null;
-
-// Konversi gender ke teks yang mudah dibaca
-switch ($gender) {
-    case 1:
-        $gender_text = "Laki-laki";
-        break;
-    case 2:
-        $gender_text = "Perempuan";
-        break;
-    default:
-        $gender_text = "Tidak diketahui";
-        break;
-}
-
-setlocale(LC_TIME, 'id_ID.utf8');
-$date = $teacher['birthday'] ?? null;
-$formatted_date = strftime("%d %B %Y", strtotime($date));
 ?>
 
 <!DOCTYPE html>
